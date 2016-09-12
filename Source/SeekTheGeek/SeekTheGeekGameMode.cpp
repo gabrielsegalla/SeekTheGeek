e46b554c549263f60033fa1a23c0bf51f1ce1e75
Gabriel Rodrigues Segalla <gabriel_segalla@estudante.sc.senai.br>
2016-09-12 17:30:35 -0300

    commit inicial hardcore

---
+++ Source/SeekTheGeek/SeekTheGeekGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SeekTheGeek.h"
+#include "SeekTheGeekGameMode.h"
+
+
+
+
---
+++ Source/SeekTheGeek/SeekTheGeekGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SeekTheGeekGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEEKTHEGEEK_API ASeekTheGeekGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
